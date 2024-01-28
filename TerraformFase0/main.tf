1aaadcc89a7237e9389c090bbaf7e1bde3e43979
Roballed <Roballed@gmail.com>
2024-01-28 18:23:53 +0100

    segundo commit fase 0

--- TerraformFase0/main.tf
+++ TerraformFase0/main.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "sg1" {
 
 ingress {
     from_port   = 80
-    to_port     = 80
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
