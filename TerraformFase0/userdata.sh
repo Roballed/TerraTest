9eb11fe4ea2663bfe26d71be6174b989207b3a60
Roballed <Roballed@gmail.com>
2024-01-28 19:04:04 +0100

    modificando el docker compose

--- TerraformFase0/userdata.sh
+++ TerraformFase0/userdata.sh
@@ -4,4 +4,5 @@ sudo apt-get update -y
 sudo apt-get install -y docker.io
 git clone https://github.com/Roballed/docker-frontend-backend-db.git
 cd docker-frontend-backend-db
-docker-compose up -d
+sudo apt-get install docker-compose -y
+sudo docker-compose up
